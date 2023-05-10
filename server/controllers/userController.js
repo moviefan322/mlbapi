7439a7653ba2e497738207ee0b2dc5e5b8acfdb8
moviefan322 <philman202@yahoo.com>
2023-05-10 11:33:11 -0400

    adds register user functionality(backend)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 // @desc    Register a new user
 // @route   POST /users
@@ -6,12 +8,46 @@ const asyncHandler = require("express-async-handler");
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
+  const userExists = await User.findOne({ email });
+
+  // Check if user exists
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Check if all fields are filled out
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please fill out all fields");
   }
 
-  res.send("success");
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create new user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    isAdmin: false,
+    accountBalance: 100,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      accountBalance: user.accountBalance,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc    Login a user
