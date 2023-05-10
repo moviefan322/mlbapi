e2987db21e19ad1fcd4e4ec70c416e0d0031d0a8
moviefan322 <philman202@yahoo.com>
2023-05-10 11:22:41 -0400

    adds userModel.js

---
+++ server/models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    accountBalance: {
+      type: Number,
+      required: true,
+      default: 100,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
