5f415970b58354bed1f4dcb2fae0307789f78fba
moviefan322 <philman202@yahoo.com>
2023-05-10 12:37:20 -0400

    adds some styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import baseball from "../assets/baseball.png";
 
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">Games</Link>
+        <Link to="/">
+          <img src={baseball} alt="baseball" height="36px" width="40px" />
+        </Link>
       </div>
       <ul>
         <li>
