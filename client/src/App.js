8ac8550f529a84db655ed9adaf53b598a225bd63
moviefan322 <philman202@yahoo.com>
2023-05-10 12:14:37 -0400

    adds routing for home, register, login

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <div>Sports Tracker</div>;
+  return (
+    <>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
