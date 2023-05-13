b57226d2874f7737793a780f48e3edeb76c0f0ba
moviefan322 <philman202@yahoo.com>
2023-05-13 14:14:09 -0400

    fills out Bets.jsx

---
+++ client/src/components/BetItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function BetItem() {
+    
+  return <div>Bet Item</div>;
+}
+
+export default BetItem;
--- client/src/pages/Bets.jsx
+++ client/src/pages/Bets.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getBets } from "../features/bet/betSlice";
+import BetItem from "../components/BetItem";
+import Spinner from "../components/Spinner";
 
 function Bets() {
-  return <div>Your bets here</div>;
+  const { bets, isLoading } = useSelector((state) => state.bet);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBets());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <h1>Bets</h1>
+      <div className="bets">
+        <div className="bet-headings">
+          <div>Date</div>
+          <div>Game</div>
+          <div>Bet</div>
+          <div>Status</div>
+          <div>$</div>
+        </div>
+        {bets.map((bet) => (
+          <BetItem key={bet._id} bet={bet} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Bets;
