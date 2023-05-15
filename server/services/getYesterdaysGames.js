ccdfc173cdd519b21124a6f5b7a298f2fb7ca9dc
moviefan322 <philman202@yahoo.com>
2023-05-15 19:36:06 -0400

    writes util to save yesterdays games

---
+++ server/services/getYesterdaysGames.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const path = require("path");
+const { writeFile } = require("fs/promises");
+const { write } = require("fs");
+
+const getYesterdaysGames = async () => {
+  const date = new Date();
+  const yesterday = new Date(date.setDate(date.getDate() - 1))
+    .toISOString()
+    .slice(0, 10);
+  const url = `http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&startDate=${yesterday}&endDate=${yesterday}`;
+
+  const { data } = await axios.get(url);
+  return data.dates[0].games;
+};
+
+const writeYesterdaysGames = async () => {
+  const games = await getYesterdaysGames();
+  await writeFile(
+    path.join(__dirname, "../devData/yesterdaysGames.json"),
+    JSON.stringify(games),
+    {
+      encoding: "utf8",
+      flag: "w",
+      mode: 0o666,
+    },
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Yesterdays games written successfully\n");
+      }
+    }
+  );
+};
+
+writeYesterdaysGames();
+
+module.exports = { getYesterdaysGames, writeYesterdaysGames };
