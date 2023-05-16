d59b3fa3add5d67c72385ed035668b8ab6446454
moviefan322 <philman202@yahoo.com>
2023-05-16 03:10:55 -0400

    batting linescore styled

---
+++ client/src/components/BattingLinescoreHome.jsx
@@ -0,0 +1,282 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function BattingLinescoreHome({ boxscore }) {
+  const homeBatters = boxscore.liveData.boxscore.teams.home.batters.filter(
+    (batter) =>
+      boxscore.liveData.plays.allPlays.filter(
+        (play) =>
+          play.matchup.batter.id === batter &&
+          play.result.type === "atBat" &&
+          play.result.eventType !== "sac_fly" &&
+          play.result.eventType !== "sac_bunt" &&
+          play.result.eventType !== "pickoff_1b" &&
+          play.result.eventType !== "pickoff_2b" &&
+          play.result.eventType !== "pickoff_3b" &&
+          play.result.eventType !== "batter_timeout" &&
+          play.result.eventType !== "walk" &&
+          play.result.eventType !== "hit_by_pitch"
+      ).length > 0
+  );
+  return (
+    <>
+      <table className="battingbox">
+        <thead>
+          <tr>
+            <th></th>
+            <th>AB</th>
+            <th>R</th>
+            <th>H</th>
+            <th>RBI</th>
+            <th>BB</th>
+            <th>SO</th>
+            <th>BA</th>
+          </tr>
+        </thead>
+        <tbody>
+          {boxscore.liveData.boxscore.teams.home.batters
+            .filter(
+              (batter) =>
+                boxscore.liveData.plays.allPlays.filter(
+                  (play) =>
+                    play.matchup.batter.id === batter &&
+                    play.result.type === "atBat" &&
+                    play.result.eventType !== "sac_fly" &&
+                    play.result.eventType !== "sac_bunt" &&
+                    play.result.eventType !== "pickoff_1b" &&
+                    play.result.eventType !== "pickoff_2b" &&
+                    play.result.eventType !== "pickoff_3b" &&
+                    play.result.eventType !== "batter_timeout" &&
+                    play.result.eventType !== "walk" &&
+                    play.result.eventType !== "hit_by_pitch"
+                ).length > 0
+            )
+            .map((batter, index) => (
+              <tr key={index}>
+                <td className="batterName">
+                  <span>{`${
+                    boxscore.liveData.boxscore.teams.home.players[
+                      "ID" + `${batter}`
+                    ].jerseyNumber
+                  }`}</span>
+                  <span>
+                    {
+                      `${
+                        boxscore.liveData.boxscore.teams.home.players[
+                          "ID" + `${batter}`
+                        ].person.fullName
+                      }`.split(" ")[1]
+                    }
+                  </span>
+                  <span>{`${
+                    boxscore.liveData.boxscore.teams.home.players[`ID${batter}`]
+                      .position.abbreviation
+                  }`}</span>
+                </td>
+                <td>
+                  {
+                    boxscore.liveData.plays.allPlays.filter(
+                      (play) =>
+                        play.matchup.batter.id === batter &&
+                        play.result.type === "atBat" &&
+                        play.result.eventType !== "sac_fly" &&
+                        play.result.eventType !== "sac_bunt" &&
+                        play.result.eventType !== "pickoff_1b" &&
+                        play.result.eventType !== "pickoff_2b" &&
+                        play.result.eventType !== "pickoff_3b" &&
+                        play.result.eventType !== "batter_timeout" &&
+                        play.result.eventType !== "walk" &&
+                        play.result.eventType !== "hit_by_pitch"
+                    ).length
+                  }
+                </td>
+                <td>
+                  {" "}
+                  {
+                    boxscore.liveData.plays.allPlays.filter((play) =>
+                      play.result.description.includes(
+                        `${
+                          boxscore.liveData.boxscore.teams.home.players[
+                            "ID" + `${batter}`
+                          ].person.fullName
+                        } scores`
+                      )
+                    ).length
+                  }
+                </td>
+                <td>
+                  {
+                    boxscore.liveData.plays.allPlays.filter(
+                      (play) =>
+                        play.matchup.batter.id === batter &&
+                        (play.result.eventType === "home_run" ||
+                          play.result.eventType === "single" ||
+                          play.result.eventType === "double" ||
+                          play.result.eventType === "triple")
+                    ).length
+                  }
+                </td>
+                <td>
+                  {" "}
+                  {boxscore.liveData.plays.allPlays.reduce(
+                    (totalRBIs, play) => {
+                      if (
+                        play.matchup.batter.id === batter &&
+                        play.result.rbi > 0
+                      ) {
+                        return totalRBIs + play.result.rbi;
+                      } else {
+                        return totalRBIs;
+                      }
+                    },
+                    0
+                  )}
+                </td>
+                <td>
+                  {
+                    boxscore.liveData.plays.allPlays.filter(
+                      (play) =>
+                        play.matchup.batter.id === batter &&
+                        play.result.eventType === "walk"
+                    ).length
+                  }
+                </td>
+                <td>
+                  {
+                    boxscore.liveData.plays.allPlays.filter(
+                      (play) =>
+                        play.matchup.batter.id === batter &&
+                        play.result.eventType === "strikeout"
+                    ).length
+                  }
+                </td>
+                <td>{`${
+                  boxscore.liveData.boxscore.teams.home.players[`ID${batter}`]
+                    .seasonStats.batting.avg
+                }`}</td>
+              </tr>
+            ))}
+          <tr>
+            <td>
+              <strong>Totals:</strong>
+            </td>
+            <td>
+              {" "}
+              {homeBatters.reduce((totalABs, batter) => {
+                const batterABs = boxscore.liveData.plays.allPlays.filter(
+                  (play) =>
+                    play.matchup.batter.id === batter &&
+                    play.result.type === "atBat" &&
+                    play.result.eventType !== "sac_fly" &&
+                    play.result.eventType !== "sac_bunt" &&
+                    play.result.eventType !== "pickoff_1b" &&
+                    play.result.eventType !== "pickoff_2b" &&
+                    play.result.eventType !== "pickoff_3b" &&
+                    play.result.eventType !== "batter_timeout" &&
+                    play.result.eventType !== "walk" &&
+                    play.result.eventType !== "hit_by_pitch"
+                ).length;
+
+                return totalABs + batterABs;
+              }, 0)}
+            </td>
+            <td>
+              {homeBatters.reduce((totalRuns, batter) => {
+                const batterRuns = boxscore.liveData.plays.allPlays.filter(
+                  (play) =>
+                    play.result.description.includes(
+                      `${
+                        boxscore.liveData.boxscore.teams.home.players[
+                          "ID" + `${batter}`
+                        ].person.fullName
+                      } scores`
+                    )
+                ).length;
+
+                return totalRuns + batterRuns;
+              }, 0)}
+            </td>
+            <td>
+              {homeBatters.reduce((totalHits, batter) => {
+                const batterHits = boxscore.liveData.plays.allPlays.filter(
+                  (play) =>
+                    play.matchup.batter.id === batter &&
+                    (play.result.eventType === "home_run" ||
+                      play.result.eventType === "single" ||
+                      play.result.eventType === "double" ||
+                      play.result.eventType === "triple")
+                ).length;
+
+                return totalHits + batterHits;
+              }, 0)}
+            </td>
+            <td>
+              {homeBatters.reduce((totalRBIs, batter) => {
+                const batterRBIs = boxscore.liveData.plays.allPlays.reduce(
+                  (totalRBIs, play) => {
+                    if (
+                      play.matchup.batter.id === batter &&
+                      play.result.rbi > 0
+                    ) {
+                      return totalRBIs + play.result.rbi;
+                    } else {
+                      return totalRBIs;
+                    }
+                  },
+                  0
+                );
+
+                return totalRBIs + batterRBIs;
+              }, 0)}
+            </td>
+            <td>
+              {" "}
+              {homeBatters.reduce((totalBBs, batter) => {
+                const batterBBs = boxscore.liveData.plays.allPlays.filter(
+                  (play) =>
+                    play.matchup.batter.id === batter &&
+                    play.result.eventType === "walk"
+                ).length;
+
+                return totalBBs + batterBBs;
+              }, 0)}
+            </td>
+            <td>
+              {" "}
+              {homeBatters.reduce((totalBBs, batter) => {
+                const batterBBs = boxscore.liveData.plays.allPlays.filter(
+                  (play) =>
+                    play.matchup.batter.id === batter &&
+                    play.result.eventType === "strikeout"
+                ).length;
+
+                return totalBBs + batterBBs;
+              }, 0)}
+            </td>
+            <td>
+              {" "}
+              {(
+                homeBatters.reduce((teamAvg, batter) => {
+                  const batterAvg = parseFloat(
+                    boxscore.liveData.boxscore.teams.home.players[`ID${batter}`]
+                      .seasonStats.batting.avg
+                  );
+                  return teamAvg + batterAvg;
+                }, 0) / homeBatters.length
+              )
+                .toFixed(3)
+                .toString()
+                .substring(1)}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+BattingLinescoreHome.propTypes = {
+  boxscore: PropTypes.object.isRequired,
+};
+
+export default BattingLinescoreHome;
--- client/src/pages/Boxscore.jsx
+++ client/src/pages/Boxscore.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import teamKeys from "../utils/teamKeys";
 import { formatDate4 } from "../utils/formatTime";
+import BattingLinescoreAway from "../components/BattingLinescoreAway";
+import BattingLinescoreHome from "../components/BattingLinescoreHome";
 
 function Boxscore() {
   const [boxscore, setBoxscore] = useState([]);
@@ -181,259 +183,23 @@ function Boxscore() {
           </strong>
         </div>
       </div>
-      <h3> {teamKeys[boxscore.liveData.boxscore.teams.away.team.name].abb}</h3>
-      <div className="awayscorescontainer">
-      <table className="battingbox">
-        <thead>
-          <tr>
-            <th></th>
-            <th>AB</th>
-            <th>R</th>
-            <th>H</th>
-            <th>RBI</th>
-            <th>BB</th>
-            <th>SO</th>
-            <th>BA</th>
-          </tr>
-        </thead>
-        <tbody>
-          {boxscore.liveData.boxscore.teams.away.batters
-            .filter(
-              (batter) =>
-                boxscore.liveData.plays.allPlays.filter(
-                  (play) =>
-                    play.matchup.batter.id === batter &&
-                    play.result.type === "atBat" &&
-                    play.result.eventType !== "sac_fly" &&
-                    play.result.eventType !== "sac_bunt" &&
-                    play.result.eventType !== "pickoff_1b" &&
-                    play.result.eventType !== "pickoff_2b" &&
-                    play.result.eventType !== "pickoff_3b" &&
-                    play.result.eventType !== "batter_timeout" &&
-                    play.result.eventType !== "walk" &&
-                    play.result.eventType !== "hit_by_pitch"
-                ).length > 0
-            )
-            .map((batter, index) => (
-              <tr key={index}>
-                <td className="batterName">
-                  <span>{`${
-                    boxscore.liveData.boxscore.teams.away.players[
-                      "ID" + `${batter}`
-                    ].jerseyNumber
-                  }`}</span>
-                  <span>
-                    {
-                      `${
-                        boxscore.liveData.boxscore.teams.away.players[
-                          "ID" + `${batter}`
-                        ].person.fullName
-                      }`.split(" ")[1]
-                    }
-                  </span>
-                  <span>{`${
-                    boxscore.liveData.boxscore.teams.away.players[`ID${batter}`]
-                      .position.abbreviation
-                  }`}</span>
-                </td>
-                <td>
-                  {
-                    boxscore.liveData.plays.allPlays.filter(
-                      (play) =>
-                        play.matchup.batter.id === batter &&
-                        play.result.type === "atBat" &&
-                        play.result.eventType !== "sac_fly" &&
-                        play.result.eventType !== "sac_bunt" &&
-                        play.result.eventType !== "pickoff_1b" &&
-                        play.result.eventType !== "pickoff_2b" &&
-                        play.result.eventType !== "pickoff_3b" &&
-                        play.result.eventType !== "batter_timeout" &&
-                        play.result.eventType !== "walk" &&
-                        play.result.eventType !== "hit_by_pitch"
-                    ).length
-                  }
-                </td>
-                <td>
-                  {" "}
-                  {
-                    boxscore.liveData.plays.allPlays.filter((play) =>
-                      play.result.description.includes(
-                        `${
-                          boxscore.liveData.boxscore.teams.away.players[
-                            "ID" + `${batter}`
-                          ].person.fullName
-                        } scores`
-                      )
-                    ).length
-                  }
-                </td>
-                <td>
-                  {
-                    boxscore.liveData.plays.allPlays.filter(
-                      (play) =>
-                        play.matchup.batter.id === batter &&
-                        (play.result.eventType === "home_run" ||
-                          play.result.eventType === "single" ||
-                          play.result.eventType === "double" ||
-                          play.result.eventType === "triple")
-                    ).length
-                  }
-                </td>
-                <td>
-                  {" "}
-                  {boxscore.liveData.plays.allPlays.reduce(
-                    (totalRBIs, play) => {
-                      if (
-                        play.matchup.batter.id === batter &&
-                        play.result.rbi > 0
-                      ) {
-                        return totalRBIs + play.result.rbi;
-                      } else {
-                        return totalRBIs;
-                      }
-                    },
-                    0
-                  )}
-                </td>
-                <td>
-                  {
-                    boxscore.liveData.plays.allPlays.filter(
-                      (play) =>
-                        play.matchup.batter.id === batter &&
-                        play.result.eventType === "walk"
-                    ).length
-                  }
-                </td>
-                <td>
-                  {
-                    boxscore.liveData.plays.allPlays.filter(
-                      (play) =>
-                        play.matchup.batter.id === batter &&
-                        play.result.eventType === "strikeout"
-                    ).length
-                  }
-                </td>
-                <td>{`${
-                  boxscore.liveData.boxscore.teams.away.players[`ID${batter}`]
-                    .seasonStats.batting.avg
-                }`}</td>
-              </tr>
-            ))}
-          <tr>
-            <td>
-              <strong>Totals:</strong>
-            </td>
-            <td>
-              {" "}
-              {awayBatters.reduce((totalABs, batter) => {
-                const batterABs = boxscore.liveData.plays.allPlays.filter(
-                  (play) =>
-                    play.matchup.batter.id === batter &&
-                    play.result.type === "atBat" &&
-                    play.result.eventType !== "sac_fly" &&
-                    play.result.eventType !== "sac_bunt" &&
-                    play.result.eventType !== "pickoff_1b" &&
-                    play.result.eventType !== "pickoff_2b" &&
-                    play.result.eventType !== "pickoff_3b" &&
-                    play.result.eventType !== "batter_timeout" &&
-                    play.result.eventType !== "walk" &&
-                    play.result.eventType !== "hit_by_pitch"
-                ).length;
-
-                return totalABs + batterABs;
-              }, 0)}
-            </td>
-            <td>
-              {awayBatters.reduce((totalRuns, batter) => {
-                const batterRuns = boxscore.liveData.plays.allPlays.filter(
-                  (play) =>
-                    play.result.description.includes(
-                      `${
-                        boxscore.liveData.boxscore.teams.away.players[
-                          "ID" + `${batter}`
-                        ].person.fullName
-                      } scores`
-                    )
-                ).length;
-
-                return totalRuns + batterRuns;
-              }, 0)}
-            </td>
-            <td>
-              {awayBatters.reduce((totalHits, batter) => {
-                const batterHits = boxscore.liveData.plays.allPlays.filter(
-                  (play) =>
-                    play.matchup.batter.id === batter &&
-                    (play.result.eventType === "home_run" ||
-                      play.result.eventType === "single" ||
-                      play.result.eventType === "double" ||
-                      play.result.eventType === "triple")
-                ).length;
-
-                return totalHits + batterHits;
-              }, 0)}
-            </td>
-            <td>
-              {awayBatters.reduce((totalRBIs, batter) => {
-                const batterRBIs = boxscore.liveData.plays.allPlays.reduce(
-                  (totalRBIs, play) => {
-                    if (
-                      play.matchup.batter.id === batter &&
-                      play.result.rbi > 0
-                    ) {
-                      return totalRBIs + play.result.rbi;
-                    } else {
-                      return totalRBIs;
-                    }
-                  },
-                  0
-                );
-
-                return totalRBIs + batterRBIs;
-              }, 0)}
-            </td>
-            <td>
-              {" "}
-              {awayBatters.reduce((totalBBs, batter) => {
-                const batterBBs = boxscore.liveData.plays.allPlays.filter(
-                  (play) =>
-                    play.matchup.batter.id === batter &&
-                    play.result.eventType === "walk"
-                ).length;
-
-                return totalBBs + batterBBs;
-              }, 0)}
-            </td>
-            <td>
-              {" "}
-              {awayBatters.reduce((totalBBs, batter) => {
-                const batterBBs = boxscore.liveData.plays.allPlays.filter(
-                  (play) =>
-                    play.matchup.batter.id === batter &&
-                    play.result.eventType === "strikeout"
-                ).length;
-
-                return totalBBs + batterBBs;
-              }, 0)}
-            </td>
-            <td>
-              {" "}
-              {(
-                awayBatters.reduce((teamAvg, batter) => {
-                  const batterAvg = parseFloat(
-                    boxscore.liveData.boxscore.teams.away.players[`ID${batter}`]
-                      .seasonStats.batting.avg
-                  );
-                  return teamAvg + batterAvg;
-                }, 0) / awayBatters.length
-              )
-                .toFixed(3)
-                .toString()
-                .substring(1)}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="linescorecontainer">
+        <div className="scorescontainer">
+          <h3>
+            {" "}
+            {teamKeys[boxscore.liveData.boxscore.teams.away.team.name].abb}
+          </h3>
+          <BattingLinescoreAway boxscore={boxscore} />
+        </div>
+        <div className="pitchers"></div>
+        <div className="scorescontainer">
+        <h3>
+          {" "}
+          {teamKeys[boxscore.liveData.boxscore.teams.home.team.name].abb}
+        </h3>
+          <BattingLinescoreHome boxscore={boxscore} />
+        </div>
+        <div className="pitchers"></div>
       </div>
     </>
   );
