a702dab7b84f5f296b98b3d02cde49ccf0d0c8a0
moviefan322 <philman202@yahoo.com>
2023-05-16 02:49:48 -0400

    creates LinescoreAway component

--- client/src/components/BattingLinescore.jsx
+++ client/src/components/BattingLinescore.jsx
@@ -1,39 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Spinner from "../components/Spinner";
-import teamKeys from "../utils/teamKeys";
-import { formatDate4 } from "../utils/formatTime";
+import React from "react";
+import PropTypes from "prop-types";
 
-function Boxscore() {
-  const [boxscore, setBoxscore] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const { gamePk } = useParams();
-
-  useEffect(() => {
-    const getBoxscore = async () => {
-      let response = () => {
-        return new Promise(function (resolve, reject) {
-          fetch(
-            `https://statsapi.mlb.com/api/v1.1/game/${gamePk}/feed/live`
-          ).then((response) => {
-            resolve(response);
-          });
-        });
-      };
-      let data = await response();
-      data = await data.json();
-      setBoxscore(data);
-      setIsLoading(false);
-    };
-    getBoxscore();
-  }, []);
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-
-  console.log(boxscore);
+function BattingLinescoreAway({ boxscore }) {
   const awayBatters = boxscore.liveData.boxscore.teams.away.batters.filter(
     (batter) =>
       boxscore.liveData.plays.allPlays.filter(
@@ -50,139 +18,8 @@ function Boxscore() {
           play.result.eventType !== "hit_by_pitch"
       ).length > 0
   );
-
   return (
     <>
-      <div className="boxscore-gameinfo">
-        <p className="boxscore-daytime">
-          {formatDate4(boxscore.gameData.datetime.dateTime)}
-          <br />
-          {boxscore.gameData.datetime.time}
-        </p>
-      </div>
-      <div className="boxscore-top">
-        <div className="top">
-          <div>
-            <img
-              src={
-                teamKeys[boxscore.liveData.boxscore.teams.away.team.name].image
-              }
-              alt="away team logo"
-              height="100px"
-            />
-          </div>
-          <div className="textbox textboxaway">
-            <p>
-              <strong>
-                {teamKeys[boxscore.liveData.boxscore.teams.away.team.name].abb}
-              </strong>
-            </p>
-            <p>
-              <strong>{boxscore.liveData.linescore.teams.away.runs}</strong>
-            </p>
-            <p>
-              {boxscore.gameData.teams.away.record.leagueRecord.wins}-
-              {boxscore.gameData.teams.away.record.leagueRecord.losses}
-            </p>
-            <p>{boxscore.gameData.teams.away.record.leagueRecord.pct}</p>
-          </div>
-        </div>
-        <div className="top">
-          <div>
-            <img
-              src={
-                teamKeys[boxscore.liveData.boxscore.teams.home.team.name].image
-              }
-              alt="home team logo"
-              height="100px"
-            />
-          </div>
-          <div className="textbox">
-            <p>
-              <strong>
-                {teamKeys[boxscore.liveData.boxscore.teams.home.team.name].abb}
-              </strong>
-            </p>
-            <p>
-              <strong>{boxscore.liveData.linescore.teams.home.runs}</strong>
-            </p>
-            <p>
-              {boxscore.gameData.teams.home.record.leagueRecord.wins}-
-              {boxscore.gameData.teams.home.record.leagueRecord.losses}
-            </p>
-            <p>{boxscore.gameData.teams.home.record.leagueRecord.pct}</p>
-          </div>
-        </div>
-      </div>
-      <div className="box-outline">
-        <div className="boxbox">
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-                <th>4</th>
-                <th>5</th>
-                <th>6</th>
-                <th>7</th>
-                <th>8</th>
-                <th>9</th>
-                <th>R</th>
-                <th>H</th>
-                <th>E</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  {" "}
-                  <strong>
-                    {
-                      teamKeys[boxscore.liveData.boxscore.teams.away.team.name]
-                        .abb
-                    }
-                  </strong>
-                </td>
-                {boxscore.liveData.linescore.innings.map((inning, index) => (
-                  <td key={index}>{inning.away.runs}</td>
-                ))}
-                <td>{boxscore.liveData.linescore.teams.away.runs}</td>
-                <td>{boxscore.liveData.linescore.teams.away.hits}</td>
-                <td>{boxscore.liveData.linescore.teams.away.errors}</td>
-              </tr>
-              <tr>
-                <td>
-                  {" "}
-                  <strong>
-                    {
-                      teamKeys[boxscore.liveData.boxscore.teams.home.team.name]
-                        .abb
-                    }
-                  </strong>
-                </td>
-                {boxscore.liveData.linescore.innings.map((inning, index) => (
-                  <td key={index}>{inning.home.runs}</td>
-                ))}
-                <td>{boxscore.liveData.linescore.teams.home.runs}</td>
-                <td>{boxscore.liveData.linescore.teams.home.hits}</td>
-                <td>{boxscore.liveData.linescore.teams.home.errors}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="decision-container">
-          <strong className="decision">
-            W:{boxscore.liveData.decisions.winner.fullName.split(" ")[1]}
-          </strong>
-          <strong className="decision">
-            L:{boxscore.liveData.decisions.loser.fullName.split(" ")[1]}
-          </strong>
-        </div>
-      </div>
-      <h3> {teamKeys[boxscore.liveData.boxscore.teams.away.team.name].abb}</h3>
-      <div className="awayscorescontainer">
       <table className="battingbox">
         <thead>
           <tr>
@@ -434,9 +271,12 @@ function Boxscore() {
           </tr>
         </tbody>
       </table>
-      </div>
     </>
   );
 }
 
-export default Boxscore;
+BattingLinescoreAway.propTypes = {
+  boxscore: PropTypes.object.isRequired,
+};
+
+export default BattingLinescoreAway;
