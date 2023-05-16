44c9909390b6bbd8a746ae8b0589eed4d3188531
moviefan322 <philman202@yahoo.com>
2023-05-15 21:06:55 -0400

    fetches data for single game

--- client/src/pages/Boxscore.jsx
+++ client/src/pages/Boxscore.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 function Boxscore() {
-  return (
-    <div>
-      Boxscore
-    </div>
-  )
+  const [boxscore, setBoxscore] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { gamePk } = useParams();
+
+  useEffect(() => {
+    const getBoxscore = async () => {
+      let response = () => {
+        return new Promise(function (resolve, reject) {
+          fetch(
+            `https://statsapi.mlb.com/api/v1.1/game/${gamePk}/feed/live`
+          ).then((response) => {
+            resolve(response);
+          });
+        });
+      };
+      let data = await response();
+      data = await data.json();
+      setBoxscore(data);
+      setIsLoading(false);
+    };
+    getBoxscore();
+  }, []);
+
+  console.log(boxscore);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return <div>Boxscore for {gamePk}</div>;
 }
 
-export default Boxscore
+export default Boxscore;
