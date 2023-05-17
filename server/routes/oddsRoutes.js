a6155917af1e218ce03b2b49a82cbadc2088124b
moviefan322 <philman202@yahoo.com>
2023-05-17 03:14:38 -0400

    adds route to get xxx games

--- server/routes/oddsRoutes.js
+++ server/routes/oddsRoutes.js
@@ -4,10 +4,13 @@ const {
   getOdds,
   postScores,
   postYesterdaysScores,
+  postXXXgames
+
 } = require("../controllers/oddsController");
 
 router.get("/", getOdds);
 router.get("/scoreboard", postScores);
 router.get("/yesterdaysScores", postYesterdaysScores);
+router.get("/:date", postXXXgames)
 
 module.exports = router;
