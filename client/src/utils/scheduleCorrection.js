07104c926288328092f04bfd2b24f45195b3cf2a
moviefan322 <philman202@yahoo.com>
2023-06-17 20:13:45 -0400

    restores/unbreaks schedule.jsx

--- client/src/utils/scheduleCorrection.js
+++ client/src/utils/scheduleCorrection.js
@@ -1,18 +1,14 @@
+const keyArray = [
+  108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 133,
+  134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 158,
+];
+
 const pruneSchedule = (teamSchedule) => {
   for (let teamId in teamSchedule) {
     for (let series in teamSchedule[teamId]) {
       const seriesNumber = parseInt(series);
       if (teamSchedule[teamId][seriesNumber].length === 1) {
         delete teamSchedule[teamId][seriesNumber];
-
-        // fix keys of following series
-        for (let j = seriesNumber + 1; j < 53; j++) {
-          const seriesVariableName = `${j}`;
-          const newSeriesVariableName = `${j - 1}`;
-          teamSchedule[teamId][newSeriesVariableName] =
-            teamSchedule[teamId][seriesVariableName];
-          delete teamSchedule[teamId][seriesVariableName];
-        }
       }
     }
   }
@@ -22,8 +18,8 @@ const pruneSchedule = (teamSchedule) => {
 export const fixScheduleErrors = (schedule) => {
   delete schedule[160];
   delete schedule[159];
-  
-  pruneSchedule(schedule);
+
+  // pruneSchedule(schedule);
   // loop through each team
   for (var i = 108; i < 158; i++) {
     if (schedule[i] === undefined) {
@@ -82,31 +78,114 @@ export const fixScheduleErrors = (schedule) => {
   return newSched;
 };
 
-const fixScheduleErrors2 = (schedule) => {
+export const fixScheduleErrors2 = (schedule) => {
+  delete schedule[160];
+  delete schedule[159];
+  // schedule = pruneSchedule(schedule);
+
   for (let teamId in schedule) {
     let doubleSeries = [];
     let undefinedSeries = [];
+    let mislabeledLength = [];
+
     for (let seriesIndex in schedule[teamId]) {
       const series = schedule[teamId][seriesIndex];
       if (series.length === 0) {
-        undefinedSeries.push(seriesIndex);
+        if (Number(seriesIndex) === 52) {
+          continue;
+        }
+        undefinedSeries.push(Number(seriesIndex));
       }
+
       if (series.length > 1) {
         const PPD = checkForPPD(series);
         if (PPD) {
           continue;
         }
-        if (series.length !== series[series.length - 1].gamesInSeries) {
+        if (
+          series.length !== series[series.length - 1].gamesInSeries &&
+          series.length > 3
+        ) {
           doubleSeries.push(Number(seriesIndex));
         }
-      }
-
-      if (doubleSeries.length > 0 && undefinedSeries.length > 0) {
-        // console.log(doubleSeries, undefinedSeries);
+        if (
+          series[0].teams.away.team.id !==
+            series[series.length - 1].teams.away.team.id ||
+          series[0].teams.home.team.id !==
+            series[series.length - 1].teams.home.team.id
+        ) {
+          mislabeledLength.push(Number(seriesIndex));
+        }
       }
     }
-    console.log(doubleSeries, undefinedSeries);
+
+    if (mislabeledLength.length > 0) {
+      for (let i = 0; i < mislabeledLength.length; i++) {
+        const mislabeledLengthIndex = mislabeledLength[i];
+        const series = schedule[teamId][mislabeledLengthIndex];
+        let firstSeries = [];
+        let secondSeries = [];
+        for (let j = 0; j < series.length; j++) {
+          if (
+            series[j].teams.away.team.id === series[0].teams.away.team.id &&
+            series[j].teams.home.team.id === series[0].teams.home.team.id
+          ) {
+            firstSeries.push(series[j]);
+          } else {
+            secondSeries.push(series[j]);
+          }
+        }
+        schedule[teamId][mislabeledLengthIndex] = firstSeries;
+        schedule[teamId][mislabeledLengthIndex + 1] = secondSeries;
+      }
+    }
+
+    if (undefinedSeries.length > 0) {
+      for (let i = 0; i < undefinedSeries.length; i++) {
+        const undefinedSeriesIndex = undefinedSeries[i];
+        const doubleSeriesIndex = doubleSeries[i];
+
+        if (doubleSeriesIndex === undefinedSeriesIndex - 1) {
+          const doubleSeries = schedule[teamId][doubleSeriesIndex];
+          const lengthOfSeries =
+            doubleSeries[schedule[teamId][doubleSeriesIndex].length - 1]
+              .gamesInSeries;
+          schedule[teamId][undefinedSeriesIndex] = schedule[teamId][
+            doubleSeriesIndex
+          ].splice(lengthOfSeries, doubleSeries.length - lengthOfSeries);
+        }
+
+        if (doubleSeriesIndex === undefinedSeriesIndex + 1) {
+          const doubleSeries = schedule[teamId][doubleSeriesIndex];
+          const lengthOfSeries =
+            doubleSeries[schedule[teamId][doubleSeriesIndex].length - 1]
+              .gamesInSeries;
+          schedule[teamId][undefinedSeriesIndex] = schedule[teamId][
+            doubleSeriesIndex
+          ].splice(lengthOfSeries, doubleSeries.length - lengthOfSeries);
+        }
+
+        // for (let i = 0; i < keyArray.length; i++) {
+        //   const key = keyArray[i];
+        //   console.log(key);
+        //   const missingSeries = schedule[key].find((series) => {
+        //     if (
+        //       series[0].teams.away.team.id === teamId ||
+        //       series[0].teams.home.team.id === teamId
+        //     ) {
+        //       return series;
+        //     }
+        //   });
+        //   schedule[teamId][undefinedSeriesIndex] = missingSeries;
+        // }
+      }
+    }
   }
+
+  schedule[120][29] = schedule[140][29];
+  schedule[109][29] = schedule[134][29];
+  schedule[158][31] = schedule[144][31];
+  schedule[109][34] = schedule[136][34];
   return schedule;
 };
 
