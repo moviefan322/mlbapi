37fe463643cfda8666fa0212040b51b26bd87947
moviefan322 <philman202@yahoo.com>
2023-06-21 19:54:38 -0400

    updates warmup display for singlegame

--- client/src/components/SingleGame.jsx
+++ client/src/components/SingleGame.jsx
@@ -234,7 +234,126 @@ function SingleGame({ game, odds, user, today }) {
               <h4>PPD</h4>
             </div>
           ) : game.status.statusCode === "PW" ? (
-            "WARMUP"
+            <div className="data-preview">
+              {singleGame.gameData.probablePitchers.away ? (
+                <div className="pitch-preview-contianer">
+                  <div className="cardteam2">
+                    <p>
+                      <strong>
+                        #
+                        {
+                          singleGame.liveData.boxscore.teams.away.players[
+                            `ID${awayStarterId}`
+                          ].jerseyNumber
+                        }
+                      </strong>
+                    </p>
+                    <p>
+                      {awayStarterStats.wins}-{awayStarterStats.losses}
+                    </p>
+                    <p>{awayStarterStats.era}</p>
+                  </div>
+                  <div
+                    style={{
+                      backgroundImage: `url(https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/${singleGame.gameData?.probablePitchers.away?.id}/headshot/67/current)`,
+                      backgroundPosition: "center",
+                      backgroundSize: "contain",
+                      backgroundRepeat: "no-repeat",
+                      height: "100px",
+                      width: "68px",
+                      border: "1px solid black",
+                      borderRadius: "5px",
+                    }}
+                  ></div>
+                </div>
+              ) : (
+                <div className="pitch-preview-contianer">
+                  <div className="cardteam2">
+                    <p>
+                      <strong>???</strong>
+                    </p>
+                  </div>
+                  <div
+                    style={{
+                      backgroundImage: `url(https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/${singleGame.gameData?.probablePitchers.away?.id}/headshot/67/current)`,
+                      backgroundPosition: "center",
+                      backgroundSize: "contain",
+                      backgroundRepeat: "no-repeat",
+                      height: "100px",
+                      width: "68px",
+                      border: "1px solid black",
+                      borderRadius: "5px",
+                    }}
+                  ></div>
+                </div>
+              )}
+
+              <h3 style={{ margin: "2rem" }}>WARMUP </h3>
+              <div className="pitch-preview-contianer">
+                {singleGame.gameData.probablePitchers.home ? (
+                  <>
+                    <div
+                      style={{
+                        backgroundImage: `url(https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/${singleGame.gameData?.probablePitchers.home?.id}/headshot/67/current)`,
+                        backgroundPosition: "center",
+                        backgroundSize: "contain",
+                        backgroundRepeat: "no-repeat",
+                        height: "100px",
+                        width: "68px",
+                        border: "1px solid black",
+                        borderRadius: "5px",
+                      }}
+                    ></div>
+                    <div className="cardteam2">
+                      {singleGame.liveData.boxscore.teams.home.players[
+                        `ID${homeStarterId}`
+                      ] ? (
+                        <>
+                          <p>
+                            <strong>
+                              #
+                              {
+                                singleGame.liveData.boxscore.teams.home.players[
+                                  `ID${homeStarterId}`
+                                ]?.jerseyNumber
+                              }
+                            </strong>
+                          </p>
+                          <p>
+                            {homeStarterStats?.wins}-{homeStarterStats?.losses}
+                          </p>
+                          <p>{homeStarterStats?.era}</p>
+                        </>
+                      ) : (
+                        <p>
+                          <strong>???</strong>
+                        </p>
+                      )}
+                    </div>
+                  </>
+                ) : (
+                  <div className="pitch-preview-contianer">
+                    <div
+                      style={{
+                        backgroundImage: `url(https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/${singleGame.gameData?.probablePitchers.home?.id}/headshot/67/current)`,
+                        backgroundPosition: "center",
+                        backgroundSize: "contain",
+                        backgroundRepeat: "no-repeat",
+                        height: "100px",
+                        width: "68px",
+                        border: "1px solid black",
+                        borderRadius: "5px",
+                      }}
+                    ></div>
+                    <div className="cardteam2">
+                      <p>
+                        <strong>???</strong>
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
           ) : (
             <div className="data-preview">
               {singleGame.gameData.probablePitchers.away ? (
