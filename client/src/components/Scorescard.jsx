228021ba5ecbd03c8aa60c3d9c6bd89d0d43b342
moviefan322 <philman202@yahoo.com>
2023-09-20 11:24:21 -0400

    properly rendering plays in scorecard format

--- client/src/components/Scorescard.jsx
+++ client/src/components/Scorescard.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 const Scorescard = ({ allPlays, battingOrder }) => {
   const [cells, setCells] = useState([]);
 
+  const inningsTotal = allPlays[allPlays.length - 1].about.inning;
+
   useEffect(() => {
     const generateScorecard = (allPlays) => {
       const tempCells = [];
@@ -28,28 +30,63 @@ const Scorescard = ({ allPlays, battingOrder }) => {
     generateScorecard(allPlays);
   }, []);
 
-  const showPlay = (cell, batterIndex, column) => {
-    if (batterIndex === cell.lineUpindex) {
-      return cell.event;
-    } else {
-      return;
-    }
+  const showPlay = (cell, column, emptyRows = null) => {
+    return cell.event;
   };
 
-  console.log(battingOrder);
+  const batterFilter = (index) => {
+    let thisList = [];
+    cells.filter((cell) => {
+      if (index === cell.lineUpindex) {
+        thisList.push(cell);
+      }
+    });
+    return thisList;
+  };
+
+  const two = batterFilter(0);
+
+  const generateRows = (batIndx) => {
+    console.log("generating!");
+    const row = [];
+    const filteredCells = batterFilter(batIndx);
+    for (let inn = 1; inn < inningsTotal; inn++) {
+      const isPlay = filteredCells.filter((cell) => cell.inning === inn);
+      console.log(isPlay);
+      if (isPlay.length === 0) {
+        row.push(<td key={inn}>-</td>);
+      } else {
+        row.push(<td key={inn}>{showPlay(isPlay[0])}</td>);
+      }
+    }
+    return row;
+  };
 
   return (
     <div>
       POOP
       <table>
-        <tbody>
-          {battingOrder.map((player, i) => {
+        <thead>
+          <tr>
+            <th>Lineup</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+            <th>8</th>
+            <th>9</th>
+          </tr>
+        </thead>
+        <tbody className="testbody">
+          {battingOrder.map((player, batIndex) => {
+            console.log("Bat order", batIndex);
             return (
-              <tr key={i}>
-                <td key={i}>{battingOrder[i]}</td>
-                {cells.map((cell, index) => {
-                  return <td key={index}>{showPlay(cell, i, i+1)}</td>;
-                })}
+              <tr key={batIndex}>
+                <td>{battingOrder[batIndex]}</td>
+                {generateRows(batIndex)}
               </tr>
             );
           })}
